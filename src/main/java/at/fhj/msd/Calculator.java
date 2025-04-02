6c954c95e66b28c5fba3d95209f6034920145044
Linda Kadyrova <linda.kadyrova@edu.fh-joanneum.at>
2025-04-02 22:27:13 +0200

    TDD- fixed the factorial method

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -2,21 +2,29 @@ package at.fhj.msd;
 
 public class Calculator {
 
-  public double add(double number1, double number2){
+  public double add(double number1, double number2) {
     return number1 + number2;
   }
-  public double minus(double number1, double number2){
+
+  public double minus(double number1, double number2) {
     return number1 - number2;
   }
-  public double divide(double number1, double number2){
+
+  public double divide(double number1, double number2) {
     return number1 / number2;
   }
-  
-  public double multiply(double number1, double number2){
+
+  public double multiply(double number1, double number2) {
     return number1 * number2;
   }
 
-  public int factorial(int n){
-    return 0;
+  public int factorial(int n) {
+    if (n < 0)
+      return 0;
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+      result *= i;
+    }
+    return result;
   }
 }
